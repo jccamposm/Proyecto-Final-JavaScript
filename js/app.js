0bbc03bc79d31d410e48060bed60898f8db91779
jccamposm <jccamposm@yahoo.com>
2018-08-20 23:04:16 -0500

    Captura Clic Numericos
    Se hace la captura de los botones numéricos por medio del ratón.

---
+++ js/app.js
@@ -0,0 +1,131 @@
+var objPantalla = document.getElementById('display')
+
+var button1 = document.getElementById('1')
+button1.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "1"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "1"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button2 = document.getElementById('2')
+button2.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "2"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "2"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button3 = document.getElementById('3')
+button3.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "3"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "3"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button4 = document.getElementById('4')
+button4.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "4"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "4"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button5 = document.getElementById('5')
+button5.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "5"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "5"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button6 = document.getElementById('6')
+button6.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "6"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "6"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button7 = document.getElementById('7')
+button7.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "7"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "7"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button8 = document.getElementById('8')
+button8.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "8"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "8"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button9 = document.getElementById('9')
+button9.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "9"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "9"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
+
+var button0 = document.getElementById('0')
+button0.addEventListener('click', function(){
+  if (objPantalla.innerHTML == "0") {
+    objPantalla.innerHTML = "0"
+  } else {
+    if (objPantalla.innerHTML.length < 8) {
+        objPantalla.innerHTML = objPantalla.innerHTML + "0"
+    } else {
+        alert('Excede la cantidad permitida de caracteres')
+    }
+  }
+})
